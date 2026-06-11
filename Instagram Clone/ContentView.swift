62a0321fc65d8bdbaff56ca9804fc2ac4c7eb079
Zachary Aguon <zaguon@icloud.com>
2026-06-11 18:17:53 +1000

    Initial Commit

---
+++ Instagram Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Instagram Clone
+//
+//  Created by Zachary Aguon on 6/11/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Instagram Clone/Instagram_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Instagram_CloneApp.swift
+//  Instagram Clone
+//
+//  Created by Zachary Aguon on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Instagram_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
