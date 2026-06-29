e8b37fe1b0e1343a6d409d5eba1d1209a67ac157
ZAguon <ZackAguon@gmail.com>
2026-06-29 17:46:35 +1000

    Initial Commit

---
+++ InstagrameClone/InstagrameCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagrameCloneApp.swift
+//  InstagrameClone
+//
+//  Created by Zachary Aguon on 6/29/26.
+//
+
+import SwiftUI
+
+@main
+struct InstagrameCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
